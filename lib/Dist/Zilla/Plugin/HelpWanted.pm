ee86ab2cc0e453a0d9e190dd3edf743e5ebcdfde
Yanick Champoux <yanick@cpan.org>
2012-06-28 20:53:42 -0400

    add status tester

--- lib/Dist/Zilla/Plugin/HelpWanted.pm
+++ lib/Dist/Zilla/Plugin/HelpWanted.pm
@@ -6,11 +6,18 @@ package Dist::Zilla::Plugin::HelpWanted;
 In dist.ini:
 
     [HelpWanted]
+    positions = maintainer co-maintainer coder translator documentation tester
+
+or
+
+    [HelpWanted]
     maintainer    = 1
     co-maintainer = 1
     coder         = 1
     translator    = 1
     documentation = 1
+    tester        = 1
+    
 
 =head1 DESCRIPTION
 
@@ -20,8 +27,8 @@ distribution.
 
 =head1 CONFIGURATION OPTIONS
 
-Position that are passed to the plugin with a C<true> value are added
-to the I<help wanted> list.
+Position  are passed to the plugin either via the 
+option C<positions>, or piecemeal (see example above).
 
 The list of possible positions is:
 
@@ -37,6 +44,8 @@ The list of possible positions is:
 
 =item    documentation
 
+=item    tester
+
 =back
 
 =cut
@@ -51,19 +60,37 @@ with qw/
     Dist::Zilla::Role::InstallTool
 /;
 
-my @positions = qw/ maintainer co-maintainer coder translator documentation /;
+my @positions = qw/ 
+    maintainer 
+    co-maintainer 
+    coder 
+    translator 
+    documentation
+    tester 
+/;
 
 has [ @positions ] => (
-    is => 'ro',
+    is => 'rw',
     isa => 'Bool',
     default => 0,
+);
+
+has 'positions' => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => '',
 );
 
 sub setup_installer {
     my $self = shift;
 
+    for my $p ( split ' ', $self->positions ) {
+        eval { $self->$p(1) } or
+            die "position '$p' not recognized\n";
+    }
+
     my @open_positions = grep { $self->$_ } @positions
-        or return;
+                         or return;
 
     $self->zilla->distmeta->{x_help_wanted} = \@open_positions;
 }
